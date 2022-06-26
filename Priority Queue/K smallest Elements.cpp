2b0f7df1a52765d22b4786e61b3891f6f642f721
Manish Doley <98950901+manishdoley23@users.noreply.github.com>
2022-06-26 12:37:21 +0530

    Create K smallest Elements.cpp

---
+++ Priority Queue/K smallest Elements.cpp
@@ -0,0 +1,19 @@
+#include<queue>
+vector<int> kSmallest(int arr[], int n, int k) {
+    vector<int> ans;
+    priority_queue<int, vector<int>, less<int>> pq;
+    for(int i=0; i<k; ++i){
+        pq.push(arr[i]);
+    }
+    for(int i=k; i<n; ++i){
+        if(arr[i] < pq.top()){
+            pq.pop();
+            pq.push(arr[i]);
+        }
+    }
+    while(pq.size()){
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    return ans;
+}
