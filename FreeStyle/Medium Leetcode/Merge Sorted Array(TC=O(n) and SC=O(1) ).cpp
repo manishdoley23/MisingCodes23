f8bb8ae5e2bfdbf46fb9efdc0e229d63bc3a5394
Manish Doley <98950901+manishdoley23@users.noreply.github.com>
2022-06-29 13:51:53 +0530

    Create Merge Sorted Array(TC=O(n) and SC=O(1) ).cpp

---
+++ FreeStyle/Medium Leetcode/Merge Sorted Array(TC=O(n) and SC=O(1) ).cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/merge-sorted-array/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int size = m+n-1;
+        int i = m-1, j = n-1;
+        while(j >= 0){
+            nums1[size--] = i>=0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--]; 
+        }
+    }
+};
