8b95098fd5a10a444df2022cc4ebd7220c53c2c0
Manish Doley <98950901+manishdoley23@users.noreply.github.com>
2022-07-01 23:11:30 +0530

    Create Missing Number.cpp

---
+++ FreeStyle/Easy Leetcode/Missing Number.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/missing-number/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum1 = ( n * (n+1) )/ 2;
+        int sum2 = 0;
+        sum2 = accumulate(nums.begin(), nums.end(), sum2);
+        return sum1 - sum2;
+    }
+};
