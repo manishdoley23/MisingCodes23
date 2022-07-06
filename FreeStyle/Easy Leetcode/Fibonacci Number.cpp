af19d202e2fe138b3063be3e26c214e9f3bd12ee
Manish Doley <98950901+manishdoley23@users.noreply.github.com>
2022-07-06 13:10:09 +0530

    Update Fibonacci Number.cpp

--- FreeStyle/Easy Leetcode/Fibonacci Number.cpp
+++ FreeStyle/Easy Leetcode/Fibonacci Number.cpp
@@ -9,3 +9,19 @@ public:
         return fib(n-1) + fib(n-2);
     }
 };
+
+//DP Approach TC = O(n), SC = O(1)
+class Solution {
+public:
+    int fib(int n) {
+       if(n < 2) return n;
+        
+        int a = 0, b = 1, c;
+        for(int i=2; i<=n; ++i){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+};
